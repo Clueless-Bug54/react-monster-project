5f282b210032d433d06c5212093704d8f0907cb7
vinod jaggavarapu <geekyprogrammer96@gmail.com>
2020-06-22 10:58:11 +0530

    Updates

---
+++ precache-manifest.9664a321c1507cab17ad7d5c548b92f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "390d2baac9dc0a4e418d6f92008a20b0",
+    "url": "/react-monster-project/index.html"
+  },
+  {
+    "revision": "d7dba8259b14107f1816",
+    "url": "/react-monster-project/static/css/main.76f97b75.chunk.css"
+  },
+  {
+    "revision": "3510e6c179d0a5867b4e",
+    "url": "/react-monster-project/static/js/2.9b482bab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-project/static/js/2.9b482bab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7dba8259b14107f1816",
+    "url": "/react-monster-project/static/js/main.811ee950.chunk.js"
+  },
+  {
+    "revision": "1abe977eb75366a788a2",
+    "url": "/react-monster-project/static/js/runtime-main.61eeb79e.js"
+  }
+]);
